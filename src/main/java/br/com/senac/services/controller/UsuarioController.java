299da53b747db11e50266bb94ea0fd68c9abd5ff
TiagoSouzaGarcia <tiago.s.garcia@gmail.com>
2020-12-02 17:39:00 -0300

    atualizando Controller

--- src/main/java/br/com/senac/services/controller/UsuarioController.java
+++ src/main/java/br/com/senac/services/controller/UsuarioController.java
@@ -7,9 +7,11 @@ import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,7 +38,7 @@ public class UsuarioController {
 	@PostMapping(value = "/saveUser", produces = "application/json", consumes = "application/json")
 	@ResponseBody
 	@CrossOrigin
-	@Produces(MediaType.APPLICATION_JSON_PATCH_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public ResponseEntity<ApiResponse> saveUser(@RequestBody Usuario usuario) {
 		Usuario novoUsuario = null;
 		try {
@@ -66,10 +68,45 @@ public class UsuarioController {
 		}
 	}
 
+	@PatchMapping(value = "/updateUser", produces = "application/json", consumes = "application/json")
+	@ResponseBody
+	@CrossOrigin
+	@Produces(MediaType.APPLICATION_JSON)
+	public ResponseEntity<ApiResponse> updateUser(@RequestBody @Validated Usuario usuario) {
+		try {
+			Usuario updateNovo = usuario;
+			if (usuario == null) {
+				return ResponseEntityUtil.unprocessableResponseEntity(message.get(MessagesProperties.ENTITY_NOT_FOUND),
+						usuario);
+			} else {
+				if (updateNovo.getNome().equalsIgnoreCase("") || updateNovo.getSenha().equals("")) {
+					return ResponseEntityUtil.notFoundResponseEntity(message.get(MessagesProperties.API_UNKNOWN_FIELDS),
+							usuario);
+				} else {
+					updateNovo = dao.saveUser(usuario);
+					if (updateNovo != null) {
+
+						return ResponseEntityUtil.okResponseEntity(message.get(MessagesProperties.CLI_UPDATED));
+					} else {
+						return ResponseEntityUtil.unprocessableResponseEntity(
+								message.get(MessagesProperties.API_UNKNOWN_FIELDS), updateNovo);
+
+					}
+				}
+
+			}
+		} catch (Exception e) {
+			return ResponseEntityUtil.unprocessableResponseEntity(message.get(MessagesProperties.ENTITY_NOT_FOUND),
+					usuario);
+
+		}
+
+	}
+
 	@GetMapping(value = "/findById", produces = "application/json")
 	@ResponseBody
 	@CrossOrigin
-	@Produces(MediaType.APPLICATION_JSON_PATCH_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public ResponseEntity<ApiResponse> findById(@RequestParam("id") Long id) {
 		Usuario novoUsuario = null;
 		try {
@@ -92,7 +129,7 @@ public class UsuarioController {
 	@GetMapping(value = "/findByName", produces = "application/json")
 	@ResponseBody
 	@CrossOrigin
-	@Produces(MediaType.APPLICATION_JSON_PATCH_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public ResponseEntity<ApiResponse> findByName(@RequestParam("nome") String nome) {
 		List<Usuario> novoUsuario = null;
 		try {
@@ -115,7 +152,7 @@ public class UsuarioController {
 	@GetMapping(value = "/findAll", produces = "application/json")
 	@ResponseBody
 	@CrossOrigin
-	@Produces(MediaType.APPLICATION_JSON_PATCH_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public ResponseEntity<ApiResponse> findAll() {
 		List<Usuario> novoUsuario = null;
 		try {
@@ -134,28 +171,25 @@ public class UsuarioController {
 					novoUsuario);
 		}
 	}
-	
+
 	@DeleteMapping(value = "/delete", produces = "application/json")
 	@ResponseBody
 	@CrossOrigin
-	@Produces(MediaType.APPLICATION_JSON_PATCH_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public ResponseEntity<ApiResponse> delete(@RequestParam("id") Long id) {
 		boolean resultado = false;
 		try {
 
-			
-			if (resultado = dao.delete(id)==false) {
-				return ResponseEntityUtil.notFoundResponseEntity
-						(message.get(MessagesProperties.CLI_NOT_FOUND), null);
+			if (resultado = dao.delete(id) == false) {
+				return ResponseEntityUtil.notFoundResponseEntity(message.get(MessagesProperties.CLI_NOT_FOUND), null);
 			} else {
-				return ResponseEntityUtil.okResponseEntity
-						(message.get(MessagesProperties.CLI_DELETED));
+				return ResponseEntityUtil.okResponseEntity(message.get(MessagesProperties.CLI_DELETED));
 
 			}
 
 		} catch (Exception e) {
-			return ResponseEntityUtil.unprocessableResponseEntity
-					(message.get(MessagesProperties.ENTITY_NOT_FOUND), null);
+			return ResponseEntityUtil.unprocessableResponseEntity(message.get(MessagesProperties.ENTITY_NOT_FOUND),
+					null);
 		}
 	}
 
